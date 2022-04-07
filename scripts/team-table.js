cd46c9d4f8e8c0ae93b27a8779071ec9272cf30b
chad-shad <100750723+chad-shad@users.noreply.github.com>
2022-04-07 11:40:36 -0500

    Add files via upload

---
+++ scripts/team-table.js
@@ -0,0 +1,44 @@
+
+function TeamClick() {
+    var x = document.getElementById("Team1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function TeamClick2() {
+    var x = document.getElementById("Team2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function TeamClick3() {
+    var x = document.getElementById("Team3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function TeamClick4() {
+    var x = document.getElementById("Team4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function TeamClick5() {
+    var x = document.getElementById("Team5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
